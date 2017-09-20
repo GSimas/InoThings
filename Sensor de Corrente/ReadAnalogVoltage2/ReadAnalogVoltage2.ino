b95771dca7614983b4679c65e5e41f091dcee316
Gustavo Simas da Silva <gustavosimassilva@gmail.com>
2017-09-20 09:40:21 -0300

    Sensor de Corrente
    Codigo e biblioteca para o sensor de corrente SCT013 - EmonLib.
    Calibracao pode ser requerida

---
+++ Sensor de Corrente/ReadAnalogVoltage2/ReadAnalogVoltage2.ino
@@ -0,0 +1,19 @@
+#include "EmonLib.h" // Include Emon Library
+EnergyMonitor emon1; // Create an instance
+
+void setup()
+{
+Serial.begin(9600);
+
+emon1.current(A4, 97.5); // Current: input pin, calibration.
+}
+
+void loop()
+{
+double Irms = emon1.calcIrms(3000); // Calculate Irms only
+
+Serial.print("Potencia: ");
+Serial.print(Irms*220.0); // Apparent power
+Serial.print("\t Corrente: ");
+Serial.println(Irms); // Irms
+}
